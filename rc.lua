6b468102fe6737ab202daf2ef813bd2917ce5ea2
alexei-pav <loshapavlov@gmail.com>
2025-02-15 13:14:58 +0100

    change default terminal from kitty to xterm

--- rc.lua
+++ rc.lua
@@ -53,7 +53,7 @@ end
 beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "kitty"
+terminal = "xterm"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
